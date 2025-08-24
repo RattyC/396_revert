bfa015c8c1ab9be023af6b6a1008ff9c056eeb3d
RattyC <rattapoommaru@gmail.com>
2025-08-24 19:48:58 +0700

    Add template part

--- 396_version_control/quasar-project/src/pages/IndexPage.vue
+++ 396_version_control/quasar-project/src/pages/IndexPage.vue
@@ -8,16 +8,16 @@
       <q-input
         filled
         v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        label="Your name * please type your full name"
+        hint="Name and surname are required"
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
       <q-input
         filled
-        type="number"
+        type="number "
         v-model="age"
-        label="Your age *"
+        label="Your age * please type your real age"
         lazy-rules
         :rules="[
           val => val !== null && val !== '' || 'Please type your age',
